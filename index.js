4d715c8d968a9200e877304bd2dce32aeeafde2f
vinodkumar <vinodtalari82@gmail.com>
2026-04-13 13:08:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Product Service v1 📦');
+});
+
+app.listen(3002, '0.0.0.0', () => {
+  console.log('Running on 3002');
+});
